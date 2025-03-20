af5a2706807dd7bd2e44fda138fddd3a7dbef0f4
魏梦卓 <799754778@qq.com>
2025-03-20 14:18:41 +0800

    feat:主页面布局处理

---
+++ src/components/layout/Tabbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <van-tabbar 
+        v-model="active"
+        placeholder
+        safe-area-inset-bottom
+        active-color="#f71111"
+        inactive-color="#333"
+        route
+        :before-change="handleBeforChange">
+        <van-tabbar-item v-for="tab in tabbarStore.tabs"
+            :key="tab.alias"
+            :name="tab.alias"
+            :icon="active == tab.alias ? tab.selection_image : tab.default_image"
+            :badge="tab.is_show_number && tab.number ? tab.number : ''"
+            :to="tab.url"
+        >{{ active == tab.alias ? tab.check_title : tab.title }}</van-tabbar-item>
+    </van-tabbar>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTabbarStore } from '@/stores/modules/tabbar';
+
+const tabbarStore = useTabbarStore();
+const active = ref(0);
+const handleBeforChange = (name) => {
+    return true
+}
+
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/stores/modules/tabbar.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+export const useTabbarStore = defineStore('store-tabbar', {
+    state: () => {
+        return {
+            tabs: [{
+                alias: 'home', title: '首页', check_title: '首页', is_show_number: false, number: '',default_image: 'wap-home-o', selection_image: 'wap-home-o',url: '/',
+            },{
+                alias: 'cart', title: '购物车', check_title: '购物车', is_show_number: true, number: '10',default_image: 'shopping-cart-o', selection_image: 'shopping-cart-o',url: '/cart',
+            },{
+                alias: 'my', title: '我的', check_title: '我的', is_show_number: false, number: '',default_image: 'friends-o', selection_image: 'friends-o',url: '/my',
+            }]
+        }
+    },
+    actions: {
+        setTabs(data) {
+            this.tabs = data
+        },
+    }
+})
+
