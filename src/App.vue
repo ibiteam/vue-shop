623c381991241aa902eb5d046c78cfa88b2a7019
胡俊 <ainixiaopihai@icloud.com>
2025-03-24 18:15:38 +0800

    feat(init): cart、category、searchHistory

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
 import { RouterView } from 'vue-router'
 import $public from '@/utils/public'
 $public.initShopConfig()
+import {onMounted} from "vue";
+onMounted(() => {
+    function getCssVariableValue(variableName) {
+        return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
+    }
+    const mainColor = getCssVariableValue('--main-color')
+    let root = document.querySelector(":root")
+    let color_number = 10
+    root.style.setProperty("--main-color", mainColor)
+    while (color_number < 100) {
+        root.style.setProperty(`--main-color-${color_number}`, mainColor + color_number)
+        color_number += 10
+    }
+})
 </script>
 
 <template>
