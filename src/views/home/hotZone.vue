203ce6bf568eb1b31d1970874a5485eaedd803ea
魏梦卓 <799754778@qq.com>
2025-04-16 16:18:08 +0800

    feat(首页):装修优化

--- src/views/home/hotZone.vue
+++ src/views/home/hotZone.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="hot-zone-wrapper home-item-wrapper">
-            <common-image v-bind="{src: content.image, width: '100%', height: 'auto', radius: '0.2rem'}" />
-            <div
-                class="hot-zone-item"
-                v-for="(item, index) in content.items"
-                :key="index"
-                :style="{
-                    width: (item.width / 100) + 'rem',
-                    height: (item.height / 100) + 'rem',
-                    top: (item.y / 100) + 'rem',
-                    left: (item.x / 100) + 'rem',
-                }"
-                @click="handleOpenLink(item.url)"
-            >
-            </div>
+        <common-image v-bind="{src: content.image, width: '100%', height: 'auto', radius: '0.2rem'}" />
+        <div
+            class="hot-zone-item"
+            v-for="(item, index) in content.items"
+            :key="index"
+            :style="{
+                width: (item.width / 100) + 'rem',
+                height: (item.height / 100) + 'rem',
+                top: (item.y / 100) + 'rem',
+                left: (item.x / 100) + 'rem',
+            }"
+            @click="handleOpenLink(item.url)"
+        >
+        </div>
     </div>
 </template>
 
