d54537f8dd393f7c93d67037d1b690501fa8f0e6
王欢 <hjsgwanghuan@163.com>
2025-04-21 14:55:53 +0800

    fix:商品轮播与视频混合

--- src/views/good/Detail.vue
+++ src/views/good/Detail.vue
@@ -46,22 +46,12 @@
 				<div class="goods">
 					<!--轮播图-->
 					<div class="swiper-box">
-						<template v-if="banner.video&&banner.video.url&&goodTab == 0">
-							<div class="play-btn">
-								<img src="@/assets/images/good/good-video-play.png" @click="router.push({name:'goodPlay',query:{url:banner.video.url}})">
-							</div>
-							<van-image :src="banner.images[0]" v-if="banner&&banner.images.length">
-								<template v-slot:loading>
-									<img src="@/assets/images/common/no-pic.png" alt="">
-								</template>
-								<template v-slot:error>
-									<img src="@/assets/images/common/no-pic.png" alt="">
-								</template>
-							</van-image>
-						</template>
-						<van-swipe :autoplay="3000" indicator-color="black" @change="onChangeSwiper" v-else ref="swiperRef">
-							<van-swipe-item v-for="(item, index) in banner.images" :key="index" @click="lookBig(item)">
-								<van-image :src="item">
+						<van-swipe :autoplay="3000" indicator-color="black" @change="onChangeSwiper" ref="swiperRef">
+							<van-swipe-item v-for="(item, index) in bannerImgVideo" :key="index" @click="lookBig(item)">
+								<div class="play-btn" v-if="item.type == 'video'">
+									<img src="@/assets/images/good/good-video-play.png" @click="router.push({name:'goodPlay',query:{url:item.video_url}})">
+								</div>
+								<van-image :src="item.url">
 									<template v-slot:loading>
 										<img src="@/assets/images/common/no-pic.png" alt="">
 									</template>
@@ -71,15 +61,11 @@
 								</van-image>
 							</van-swipe-item>
 							<template #indicator>
-								<div class="indicator-wrap" v-if="banner.images&&banner.images.length>1">
-									<div class="custom-indicator" :class="{'indicator-active':swiperIndex == index}" v-for="(item, index) in banner.images" :key="`swiperIndex-${index}`"></div>
+								<div class="indicator-wrap" v-if="bannerImgVideo.length">
+									<div class="custom-indicator" :class="{'indicator-active':swiperIndex == index}" v-for="(item, index) in bannerImgVideo" :key="`swiperIndex-${index}`"></div>
 								</div>
 							</template>
 						</van-swipe>
-						<div class="main-btn-wrap">
-							<div :class="{active:goodTab == 0}" v-if="banner.video.url" @click="goodTab = 0">视频</div>
-							<div :class="{active:goodTab == 1}" @click="changeToImg">图集</div>
-						</div>
 					</div>
 					<div style="position: relative;top: -0.4rem;border-radius: 0.34rem 0.34rem 0 0;overflow: hidden;margin-bottom: -0.4rem;">
 						<!--        已删除/已下架        -->
@@ -401,6 +387,7 @@ const detailRef = ref(null)
 const commentRef = ref(null)
 // 响应式数据
 const banner = ref({})
+const bannerImgVideo = ref([])
 const evaluate = ref({
 	items: [],
 	tag_data: []
@@ -414,13 +401,12 @@ const changeAddress = (item) => {
 	addressId.value = item.id
 	selectAddress.value = `${item.province} ${item.city} ${item.district}`
 }
-const goodTab = ref(1)
+
 const swiperIndex = ref(0)
 const fromPath = ref('')
 const goodsNo = ref('')
 const opacity = ref(0)
 const active = ref('good')
-const nodata = ref(false)
 const goodsInfo = ref({})
 const isAttention = ref(false)
 const shoppingType = ref(-1)
@@ -506,14 +492,6 @@ const lookBigImg = (item, idx) => {
 	showPreviewer.value = true
 }
 
-const changeToImg = () => {
-	goodTab.value = 1
-	nextTick(() => {
-		swiperRef.value.swipeTo({index: 0})
-		swiperIndex.value = 0
-	})
-}
-
 const onChangeSwiper = (index) => {
 	swiperIndex.value = index
 }
@@ -522,9 +500,11 @@ const handleBack = () => {
 	router.back(-1)
 }
 
-const lookBig = (url) => {
+const lookBig = (imgItem) => {
+	if(imgItem.type == 'video') return
 	imgUrlBig.value = banner.value.images
-	startIndex.value = imgUrlBig.value.findIndex(item => item === url)
+	startIndex.value = imgUrlBig.value.findIndex(item => item === imgItem.url)
+	startIndex.value ++
 	showIndex.value = true
 	showPreviewer.value = true
 }
@@ -543,7 +523,6 @@ const handleScroll = () => {
 	let commentTop = commentRef.value && commentRef.value.offsetTop - 44
 	let detailTop = detailRef.value && detailRef.value.offsetTop - 44
 	let recommendTop = recommendRef.value && recommendRef.value.offsetTop - 44
-	let recommendHeight = recommendRef.value && recommendRef.value.offsetHeight
 
 	// 计算滚动距离在哪个区间，修改active.value对应的样式名
 	if (scrollTopVal < commentTop) {
@@ -668,6 +647,11 @@ const getData = () => {
 		if (isSuccessCode(res)) {
 			placeholder.value = false
 			banner.value = res.data.banner
+			bannerImgVideo.value = []
+			banner.value.images.length && banner.value.images.forEach((item, index) => {
+				bannerImgVideo.value.push({url: item, type: 'img'})
+			})
+			banner.value.video.url && bannerImgVideo.value.unshift({url: bannerImgVideo.value[0].url, type: 'video', video_url: banner.value.video.url})
 			/**商品信息**/
 			goodsInfo.value = res.data.center
 			evaluate.value = res.data.center.evaluate
@@ -692,9 +676,6 @@ const getData = () => {
 				})
 			}
 
-			if (res.data.banner.video.url) {
-				goodTab.value = 0
-			}
 			/**规格参数**/
 			goodsAttr.value = res.data.center.parameters
 			/**购物车数量**/
@@ -1101,19 +1082,16 @@ router.beforeEach((to, from, next) => {
 			}
 			.play-btn {
 				position: absolute;
-				width: 1.6rem;
-				height: 1.6rem;
+				width: 1.1rem;
+				height: 1.1rem;
 
 				img {
 					width: 100%;
 					height: 100%;
 				}
 
-				left: 0;
-				top: 0;
-				bottom: 0;
-				right: 0;
-				margin: auto;
+				left: 0.3rem;
+				bottom: 1.2rem;
 				z-index: 2;
 			}
 			.indicator-wrap {
